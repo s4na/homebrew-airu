1d6579f2a67bb14f240e7e9912bfe4288c0c1463
s4na <appletea.umauma@gmail.com>
2025-03-12 17:47:55 +0900

    Homebrewのtapプロジェクトを完成させる。READMEのUsage通りにインストールできるようにするため

--- Formula/airu.rb
+++ Formula/airu.rb
@@ -2,7 +2,7 @@ class Airu < Formula
   desc "AIルールマネージャー"
   homepage "https://github.com/s4na/homebrew-airu"
   url "https://github.com/s4na/homebrew-airu/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # 実際のリリース時に更新する必要があります
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # リリース後に実際のハッシュ値に更新する必要があります
   license "MIT"
 
   depends_on "ruby"
