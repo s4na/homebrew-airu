1980e7dfddeadc0ad52a30a359831f89d990184a
s4na <appletea.umauma@gmail.com>
2025-03-12 17:52:47 +0900

    SHA256ハッシュを実際の値に更新

--- Formula/airu.rb
+++ Formula/airu.rb
@@ -2,7 +2,7 @@ class Airu < Formula
   desc "AIルールマネージャー"
   homepage "https://github.com/s4na/homebrew-airu"
   url "https://github.com/s4na/homebrew-airu/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # リリース後に実際のハッシュ値に更新する必要があります
+  sha256 "088c5683bb86cf32148ccd1dc57f99aaced2941fb5b5dfe5b9f15c22730843d8"
   license "MIT"
 
   depends_on "ruby"
